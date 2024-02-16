7f4fc8c943309889436c5a5f4c422237b61d96b2
Just1nBon1n <152336193+Just1nBon1n@users.noreply.github.com>
2024-02-15 20:01:13 -0800

    fin partie 1

--- Assets/Scripts/DeplacementObjets.cs
+++ Assets/Scripts/DeplacementObjets.cs
@@ -13,17 +13,22 @@ public class DeplacementObjets : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        float valeurAleatoireY = Random.Range(-deplacementAleatoire, deplacementAleatoire);  // Génère une valeur aléatoire
+        transform.position = new Vector2(transform.position.x, valeurAleatoireY);	// positionnement vertical de l'objet
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform­.Translate(vitesse, 0f, 0f);
+
         if (transform.position.x < positionFin)
         {
             transform.position = new Vector3(positionDebut, transform.position.y, 0);
+            float valeurAleatoireY = Random.Range(-deplacementAleatoire, deplacementAleatoire);  // Génère une valeur aléatoire
+            transform.position = new Vector2(positionDebut, valeurAleatoireY);	// positionnement vertical de l'objet
         }
 
-        transform­.Translate(vitesse, 0f, 0f);
     }
+
 }
