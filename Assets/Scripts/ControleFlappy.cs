9b77a69f958b97a4d689a156e8702bdc5f34118e
Just1nBon1n <152336193+Just1nBon1n@users.noreply.github.com>
2024-03-04 11:40:51 -0800

    Fin Partie 4

--- Assets/Scripts/ControleFlappy.cs
+++ Assets/Scripts/ControleFlappy.cs
@@ -1,51 +1,91 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ControleFlappy : MonoBehaviour
 {
+    //Vitesse
     public float vitesseHorizontale;
     public float vitesseVerticale;
 
-    //Sprite
-    public Sprite flappyBlesse;
-    public Sprite flappyNormale;
+    //Sprite Falppy
+    public Sprite flappyNormaleBas;
+    public Sprite flappyNormaleHaut;
+    public Sprite flappyBlesseBas;
+    public Sprite flappyBlesseHaut;
+
     //Game Object
     public GameObject objetPiece;
     public GameObject objetPackVie;
     public GameObject objetChampingon;
+    
     //Audio
     public AudioClip sonCollision;
     public AudioClip sonPiece;
     public AudioClip sonPackVie;
-    public AudioClip sonChampigon;
+    public AudioClip sonChampingon;
+    public AudioClip sonFinPartie;
+
+    //FinPartie
+    public bool flappyTouchee = false;
+    public bool partieTerminee = false;
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D) || (Input.GetKey(KeyCode.RightArrow))) //Deplacement vers la droite
+        if (partieTerminee == false)
         {
-            vitesseHorizontale = 2;
-        }
-        else if (Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.LeftArrow))) //Deplacement vers la gauche 
-        {
-            vitesseHorizontale = -3;
-        }
-        else
-        {
-            vitesseHorizontale = GetComponent<Rigidbody2D>().velocity.x; //vitesse horizontale actuelle
+            //touche D
+            if (Input.GetKey(KeyCode.D) || (Input.GetKey(KeyCode.RightArrow))) //Deplacement vers la droite
+            {
+                vitesseHorizontale = 2;
+            }
+            //touche A
+            else if (Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.LeftArrow))) //Deplacement vers la gauche 
+            {
+                vitesseHorizontale = -3;
+            }
+            else
+            {
+                vitesseHorizontale = GetComponent<Rigidbody2D>().velocity.x; //vitesse horizontale actuelle
+            }
+
+            //touche W
+            if (Input.GetKeyDown(KeyCode.W) || (Input.GetKeyDown(KeyCode.UpArrow)))
+            {
+                vitesseVerticale = 5;
+                if (flappyTouchee == false)
+                {
+                    GetComponent<SpriteRenderer>().sprite = flappyNormaleBas;
+                }
+                else
+                {
+                    GetComponent<SpriteRenderer>().sprite = flappyBlesseBas;
+                }
+            }
+            else
+            {
+                vitesseVerticale = GetComponent<Rigidbody2D>().velocity.y; //vitesse horizontale actuelle
+            }
+
+            GetComponent<Rigidbody2D>().velocity = new Vector2(vitesseHorizontale, vitesseVerticale);
+
+
+            if (Input.GetKeyUp(KeyCode.W) || (Input.GetKeyUp(KeyCode.UpArrow)))
+            {
+                if (flappyTouchee == false)
+                {
+                    GetComponent<SpriteRenderer>().sprite = flappyNormaleHaut;
+                }
+                else
+                {
+                    GetComponent<SpriteRenderer>().sprite = flappyBlesseHaut;
+                }
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.W) || (Input.GetKeyDown(KeyCode.UpArrow)))
-        {
-            vitesseVerticale = 5;
-        }
-        else
-        {
-            vitesseVerticale = GetComponent<Rigidbody2D>().velocity.y; //vitesse horizontale actuelle
-        }
-
-        GetComponent<Rigidbody2D>().velocity = new Vector2(vitesseHorizontale, vitesseVerticale);
     }
 
     //Collisions
@@ -56,8 +96,27 @@ public class ControleFlappy : MonoBehaviour
         //Collision flappy et colonne
         if (collision.gameObject.name == "Colonne")
         {
-            GetComponent<SpriteRenderer>().sprite = flappyBlesse;
             GetComponent<AudioSource>().PlayOneShot(sonCollision, 1f);
+            //
+            if (flappyTouchee == false)
+            {
+                GetComponent<SpriteRenderer>().sprite = flappyBlesseBas;
+                flappyTouchee = true;
+            } 
+            else
+            {
+                partieTerminee = true;
+                //Activer rotation
+                GetComponent<Rigidbody2D>().freezeRotation = false;
+                //Desactive collider
+                GetComponent<Collider2D>().enabled = false;
+                //Angular velocity
+                GetComponent<Rigidbody2D>().angularVelocity = 100;
+                //Son finPartie
+                GetComponent<AudioSource>().PlayOneShot(sonFinPartie, 1f);
+                //Relancement
+                Invoke("RelancerPartie", 3f);
+            }
         }
 
         //Collision flappy piece
@@ -65,22 +124,26 @@ public class ControleFlappy : MonoBehaviour
         {
             collision.gameObject.SetActive(false);
             Invoke("ActivePiece", 3f);
+            GetComponent<AudioSource>().PlayOneShot(sonPiece, 1f);
         }
 
         //Collision flappy packVie
         else if(collision.gameObject.name == "PackVie")
         {
+            flappyTouchee = false;
             collision.gameObject.SetActive(false);
             Invoke("ActivePackVie", 3f);
-            GetComponent<SpriteRenderer>().sprite = flappyNormale;
+            GetComponent<SpriteRenderer>().sprite = flappyNormaleBas;
+            GetComponent<AudioSource>().PlayOneShot(sonPackVie, 1f);
         }
 
-        //Collision flappy champigon
+        //Collision flappy champingon
         else if(collision.gameObject.name == "Champingon")
         {
             collision.gameObject.SetActive(false);
             Invoke("ActiveChampingon", 3f);
             transform.localScale *= 1.5f;
+            GetComponent<AudioSource>().PlayOneShot(sonChampingon, 1f);
         }
     }
 
@@ -90,7 +153,7 @@ public class ControleFlappy : MonoBehaviour
     void ActivePiece()
     {
         objetPiece.SetActive(true);
-        float deplacementAleatoire = Random.Range(-1, 1);
+        float deplacementAleatoire = Random.Range(-2, 2);
         objetPiece.transform.position = new Vector2(objetPiece.transform.position.x, deplacementAleatoire);	// deplacement aleatoire verticale piece
     }
 
@@ -98,6 +161,8 @@ public class ControleFlappy : MonoBehaviour
     void ActivePackVie()
     {
         objetPackVie.SetActive(true);
+        float deplacementAleatoire = Random.Range(-1, 1);
+        objetPackVie.transform.position = new Vector2(objetPackVie.transform.position.x, deplacementAleatoire);	// deplacement aleatoire verticale pack vie
     }
 
     //fonction pour reactiver le champigon
@@ -105,5 +170,13 @@ public class ControleFlappy : MonoBehaviour
     {
         objetChampingon.SetActive(true);
         transform.localScale /= 1.5f;
+        float deplacementAleatoire = Random.Range(-1, 1);
+        objetChampingon.transform.position = new Vector2(objetChampingon.transform.position.x, deplacementAleatoire);	// deplacement aleatoire verticale Champingon
+    }
+
+    //Fonction pour redemarer le jeu
+    void RelancerPartie()
+    {
+        SceneManager.LoadScene(0);
     }
 }
